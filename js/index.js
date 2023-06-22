1d85b021913b63e46602d37b9569ffdf110bba61
Nicko Cruz M <nickocruzm@gmail.com>
2023-06-22 16:31:55 -0700

    Part 3

--- js/index.js
+++ js/index.js
@@ -1,15 +1,39 @@
 fetch('https://data.cdc.gov/resource/w9j2-ggv5.csv')
    .then(function (response) {
-      return response.text();
+    return response.text();
    })
-   .then(function (text) {
-	csvToSeries(text);
+   .then(function (text){
+    let series = csvToSeries(text);
+    renderChart(series);
    })
    .catch(function (error) {
-      console.log(error);
+    console.log(error);
    });
 
 function csvToSeries(text) {
-   console.log(text);
+    const lifeExp = 'average_life_expectancy';
+    let dataAsJson = JSC.csv2Json(text);
+    let male = [], female = [];
+    dataAsJson.forEach(function (row) {
+        //add either to male, female, or discard.
+        if (row.race === 'All Races') {
+            if (row.sex === 'Male') {
+                male.push({x: row.year, y: row[lifeExp]});
+            } else if (row.sex === 'Female') {
+                female.push({x: row.year, y: row[lifeExp]});
+            }
+        }
+    });
+    return [
+        {name: 'Male', points: male},
+        {name: 'Female', points: female}
+    ];
+    
+}
+
+function renderChart(series){
+    JSC.Chart('chartDiv',{
+        series: series
+    });
 }
 
