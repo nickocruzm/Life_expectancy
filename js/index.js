e11add08a006f031e823bfe0d0b0f32b0f22ddbd
Nicko Cruz M <nickocruzm@gmail.com>
2023-06-22 14:54:38 -0700

    Part 2

--- js/index.js
+++ js/index.js
@@ -1,22 +1,3 @@
-JSC.Chart('chartDiv', {
-    type: 'horizontal column',
-    series: [
-        {
-            name:'Andy',
-            points: [
-                {x: 'Apples', y:50},
-                {x: 'Oranges', y:42}
-            ]
-        },{
-            name:'Anna',
-            points:[
-                {x: 'Apples', y: 30},
-                {x: 'Oranges', y: 22}
-            ]
-        }
-    ]
-});
-
 fetch('https://data.cdc.gov/resource/w9j2-ggv5.csv')
    .then(function (response) {
       return response.text();
