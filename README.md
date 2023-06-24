e24f403cf7b90797db6b5a3f00cbe23564db85f9
Nicko Cruz M <nickocruzm@gmail.com>
2023-06-24 16:02:15 -0700

    additonal graph added

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Life_expectancy
 
-Using JSCharting and data collected by NCHS, to produce a simple chart. 
+Using JSCharting and data collected by National Center Health Statistics (NCHS). <https://www.cdc.gov/nchs/index.htm>
+
+Simple Line chart comparing life expectancy averages by Gender
+![Genders](by_gender.png)
+
+Simple Line chart comparing life expectancy averages by Race
+![Races](by_race.png)
--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ fetch('https://data.cdc.gov/resource/w9j2-ggv5.csv')
    })
    .then(function (text){
     let series = csvToSeries(text);
-    renderChart(series);
+    renderCharts(series);
     console.log(series)
    })
    .catch(function (error) {
@@ -39,13 +39,30 @@ function csvToSeries(text) {
     });
     return [
         {name: 'Black', points: Black},
-        {name: 'While', points: White}
-    ];
+        {name: 'White', points: White},
+        {name: 'Male', points: Male},
+        {name: 'Female', points: Female}
+    ]
     
 }
 
-function renderChart(series){
-    JSC.Chart('chartDiv',{
+function renderCharts(series){
+
+    JSC.Chart('chartDivA',{
+        title_label_text: 'Life Expectancy in the United States',
+        annotations: [{
+            label_text: 'Source: National Center for Health Statistics',
+            position: 'bottom left'
+        }],
+        legend_template: '%icon,%name',
+        legend_visible: true,
+        defaultSeries_lastPoint_label_test: '<b>%seriesName</b>',
+        xAxis_crosshair_enabled: true,
+        //xAxis: {crosshari: {enabled: true}},
+        defaultPoint_tooltip: '%seriesName <b>%yValue</b> years', 
+        series: [series[0],series[1]]
+    });
+    JSC.Chart('chartDivB',{
         title_label_text: 'Life Expectancy in the United States',
         annotations: [{
             label_text: 'Source: National Center for Health Statistics',
@@ -57,7 +74,8 @@ function renderChart(series){
         xAxis_crosshair_enabled: true,
         //xAxis: {crosshari: {enabled: true}},
         defaultPoint_tooltip: '%seriesName <b>%yValue</b> years',
-        series: series
+        series: [series[2],series[3]]
     });
+
 }
 
